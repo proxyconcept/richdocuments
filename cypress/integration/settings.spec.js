214a9ab710d779a1785e7a6e391a68430c22b387
Julius Härtl <jus@bitgrid.net>
2022-03-03 10:46:21 +0100

    Settings test adjust to protocol
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- cypress/integration/settings.spec.js
+++ cypress/integration/settings.spec.js
@@ -23,13 +23,31 @@
 import { randHash } from '../utils/'
 const randUser = randHash()
 
+const usesHttps = Cypress.env('baseUrl').substr(0, 5) === 'https'
+const collaboraUrl= Cypress.env('collaboraUrl')
+
 describe('Files default view', function() {
+	before(function() {
+	})
 	beforeEach(function () {
 		cy.login('admin', 'admin', '/settings/admin/richdocuments')
+		cy.intercept({
+		  method: 'POST',
+		  url: '/index.php/apps/richdocuments/ajax/admin.php',
+		}).as('updateSettings');
 	})
 
 	it('Opens settings', function() {
 		cy.scrollTo('topLeft')
+
+		cy.get('#app-content')
+			.scrollIntoView()
+			.should('be.visible')
+		cy.screenshot()
+		cy.get('#wopi_url')
+			.clear()
+			.type(collaboraUrl + '{enter}')
+		cy.wait('@updateSettings').its('response.statusCode').should('equal', 200)
 		cy.get('#security-warning-state-ok .message')
 			.should('be.visible')
 			.should('contain.text', 'Collabora Online server is reachable.')
@@ -41,9 +59,13 @@ describe('Files default view', function() {
 		cy.get('#secure-view-settings')
 			.scrollIntoView()
 			.should('be.visible')
+
+		// FIXME: Template settings only get visible after reload
+		cy.reload()
 		cy.get('#richdocuments-templates')
 			.scrollIntoView()
 			.should('be.visible')
+
 	})
 
 	it('Error for invalid url', function() {
@@ -53,7 +75,8 @@ describe('Files default view', function() {
 		cy.screenshot()
 		cy.get('#wopi_url')
 			.clear()
-			.type('http://invalid.example.com<enter>')
+			.type((usesHttps ? 'https' : 'http') + '://invalid.example.com{enter}')
+		cy.wait('@updateSettings').its('response.statusCode').should('equal', 500)
 		cy.get('#security-warning-state-failure .message')
 			.should('be.visible')
 			.should('contain.text', 'Could not establish connection to the Collabora Online server.')
