215b40b63f540bcc4a1c9eaf2d2f2e6f6e4fb350
Julius Härtl <jus@bitgrid.net>
2022-03-10 19:03:38 +0100

    Adjust phpunit config
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,11 +3,7 @@
 if (!defined('PHPUNIT_RUN')) {
 	define('PHPUNIT_RUN', 1);
 }
+require_once __DIR__ . '/../../../tests/bootstrap.php';
 
-require_once __DIR__.'/../../../lib/base.php';
-
-if(!class_exists('\PHPUnit\Framework\TestCase')) {
-	require_once('PHPUnit/Autoload.php');
-}
 \OC_App::loadApp('richdocuments');
 OC_Hook::clear();
